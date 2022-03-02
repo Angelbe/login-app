27d27770fc57b2fcd3cdcc37aed5eba06f2d6614
Angel Bernardo <bernard.angel93@gmail.com>
2022-03-02 23:37:50 +0100

    Head and favicon for the app

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,11 +4,12 @@ import Header from "src/components/Header";
 import Footer from "src/components/Footer";
 import Main from "src/components/Main";
 import LoginForm from "src/components/LoginForm";
-
 import { PageStyled } from "styles/globalStyledComponents";
+import HeadApp from "src/components/HeadApp";
 
 const Login: NextPage = () => (
   <PageStyled>
+    <HeadApp />
     <Header />
     <Main className="MainLogin">
       <LoginForm />
